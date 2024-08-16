5e209d5e97a54e6190b6beb4c34612aa83b0e20d
Ryan <57645738+RMoore35@users.noreply.github.com>
2024-08-16 13:21:25 -0300

    Update treasurehunt.js
    Added TODO items

--- treasurehunt.js
+++ treasurehunt.js
@@ -15,15 +15,18 @@ const treasureFinder = (location) => {
 
     if(location === bombLocation) {
         document.getElementById(location).innerHTML = "&#x2620"
+        // TODO: Need to show whole map
         alert("You lose!")
         // window.addEventListener('click',()=>{ window.location.reload()})
         window.location.reload()
     } else if(location === treasLocation) {
         document.getElementById(location).innerHTML = "&#x1f308"
+        // TODO: Need to show whole map
         alert("You win!")
         // window.addEventListener('click',()=>{ window.location.reload()})
         window.location.reload()
     } else{
+        // TODO: Need to fix counter. Set a value and count down
         document.getElementById(location).innerHTML = "&#x1f332"
         clickCounter += 1
         countEl.innerHTML = clickCounter
